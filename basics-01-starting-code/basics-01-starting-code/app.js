082af901688bc01f86dbbecec8a80fa0bc2f12ac
deokjeong <sp10051993@gmail.com>
2023-03-09 22:11:06 +0900

    2day or  v-bind & v-html

--- basics-01-starting-code/basics-01-starting-code/app.js
+++ basics-01-starting-code/basics-01-starting-code/app.js
@@ -1,8 +1,21 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoal: '완성했다 뷰!'
+            courseGoal: '완성했다 뷰!',
+            vueLink : 'https://naver.com',
+            courseGoalA : 'Finish the course and learn Vue!',
+            courseGoalB : '<h2 class="color_pink">Master Vue and build amazing apps!</h2>'
         };
+    },
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.random();
+            if ( randomNumber < 0.5) {
+                return this.courseGoalA;
+            } else {
+                return this.courseGoalB;
+            }
+        }
     }
 });
 app.mount('#user-goal');
