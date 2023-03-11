293f681ab4b211f5164ad568f87b4b923e4a67f1
deokjeong <sp10051993@gmail.com>
2023-03-11 16:51:30 +0900

    3day v-on & v-on:input & v-on:keyup.enter & v-once

--- basics-03-events-starting-code/basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/basics-03-events-starting-code/app.js
@@ -2,9 +2,24 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      inputName: '',
+      name: '',
+      confirmedName: ''
     };
   },
   methods: {
+    ketInputName() {
+      this.confirmedName = event.target.value;
+    },
+    submitForm(event) {
+      alert('Submitted!');
+    },
+    setInputName() {
+      this.inputName = event.target.value;
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + ' ' + lastName;
+    },
     add(num) {
       this.counter = this.counter + num;
       // 더 짧은 방법 사용 가능
@@ -14,7 +29,7 @@ const app = Vue.createApp({
       this.counter = this.counter - num;
       // 더 짧은 방법 사용 가능
       // this.counter--;
-    }
+    },
   }
 });
 
