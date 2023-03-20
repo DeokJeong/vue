5c18480fba9815656a7f6ef0298677c8d561cd34
deokjeong <sp10051993@gmail.com>
2023-03-20 21:46:14 +0900

    9day Vue기본 & 컴포먼트 & 스타일링

---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">{{detailsAreVisible ? 'Hide' : 'Show'}} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone : </strong> {{ friend.phone }}</li>
+      <li><strong>Email : </strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: 'kimdara01',
+        name: '김 다라',
+        phone: '010 1234 5678',
+        email: 'kimdara@test.com'
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+};
+</script>
